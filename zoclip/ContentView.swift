ac14d9183896f99db189084fe90248a2fb3714a3
zhihaofans <zhihaofans@hotmail.com>
2025-12-03 23:34:46 +0800

    Initial Commit

---
+++ zoclip/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  zoclip
+//
+//  Created by zzh on 2025/12/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ zoclip/zoclipApp.swift
@@ -0,0 +1,17 @@
+//
+//  zoclipApp.swift
+//  zoclip
+//
+//  Created by zzh on 2025/12/3.
+//
+
+import SwiftUI
+
+@main
+struct zoclipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
